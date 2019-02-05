069ff844d85372720fcbe2c18d671ea14d645153
aonomike <aonomike@gmail.com>
2019-02-05 11:31:41 +0300

    Add authentication functionality

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -4,14 +4,12 @@ defmodule Discuss.AuthController do
 
     alias Discuss.User
 
-    def callback(%{assigns: %{ueber_auth: auth}} = conn, params) do
-        IO.puts("*****************************************")
-        IO.inspect(auth)
-        # user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
-    #    changeset = User.changeset(%User{}, user_params) 
-       
-    #    signin(conn, changeset)
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+        user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+        changeset = User.changeset(%User{}, user_params)
+        signin(conn, changeset)
     end
+    
 
     defp signin(conn, changeset) do
         case insert_or_update_user(changeset) do
@@ -27,8 +25,8 @@ defmodule Discuss.AuthController do
         end   
     end
 
-    defp insert_or_update_user(changeset) do
-        case Repo.get_by(User, changeset.changes.email) do
+    defp insert_or_update_user(changeset) do       
+        case Repo.get_by(User, email: changeset.changes.email) do
             nil ->
                 Repo.insert(changeset)
             user ->
